6dd28b455f783df41cf5b732b5ef073a15cba46c
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-07-19 19:14:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class Person {
 		this.jobTitle = jobTitle;
 		}
 	jobGreet(){
-		console.log(`Hello,my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 	}
 
